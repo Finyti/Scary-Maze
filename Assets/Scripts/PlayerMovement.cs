a01d964315914304a1434059163e52eb00e649e7
Finyti <122672171+Finyti@users.noreply.github.com>
2023-10-22 13:46:49 +0300

    LightAndMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static Unity.VisualScripting.Member;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Wall") || collision.gameObject.name.Contains("Walls"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+
+    }
+}
